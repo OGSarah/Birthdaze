051f123dea7f7710378f7619928d1822bbbdc012
Sarah Clark <sarahireneclark@gmail.com>
2025-08-20 23:18:41 -0500

    Initial commit of the project

---
+++ Birthdaze/Birthdaze/BirthdazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdazeApp.swift
+//  Birthdaze
+//
+//  Created by Sarah Clark on 8/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
