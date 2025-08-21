a172775e33be4f227206185023e8246c2f6f6d32
Sarah Clark <sarahireneclark@gmail.com>
2025-08-21 17:37:14 -0500

    Added "Add new birthday" section of UI

--- Birthdaze/Birthdaze/ContentView.swift
+++ Birthdaze/Birthdaze/ContentView.swift
@@ -20,6 +20,9 @@ struct ContentView: View {
         Friend(name: "Declan", birthday: Self.createDate(year: 2017, month: 4, day: 18))
     ]
 
+    @State private var newName = ""
+    @State private var newDate = Date.now
+
     var body: some View {
         NavigationStack {
             List(friends, id: \.name) { friend in
@@ -30,6 +33,29 @@ struct ContentView: View {
                 }
             }
              .navigationTitle("Birthdays")
+             .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+
+                        newName = ""
+                        newDate = .now
+                    }
+                    .foregroundStyle(.white)
+                    .padding(10)
+                    .bold()
+                    .glassEffect(.regular.tint(.blue).interactive())
+                }
+                .padding()
+                .background(.bar)
+             }
         }
 
     }
